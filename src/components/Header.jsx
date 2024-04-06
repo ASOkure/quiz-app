b9c543052885be08fa9f593f9cf3fe829dd50017
Akanimo Sam Okure <akan.okure@gmail.com>
2024-04-06 21:42:55 +0100

    initial setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logoImg from "../assets/Quiz-logo.png;";
+export default function Header() {
+  return (
+    <header>
+      <img src={logoImg} />
+
+      <h1> Quiz</h1>
+    </header>
+  );
+}
